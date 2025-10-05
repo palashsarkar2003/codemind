eefa497f1e55f7b88f46d08a461f9f7c59515672
palashsarkar07 <psarkar5683@gmail.com>
2025-10-05 23:21:52 +0530

    updated port

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv"
 import app from "./src/app.js"
 dotenv.config()
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server is running at port 3000");
     
 })
