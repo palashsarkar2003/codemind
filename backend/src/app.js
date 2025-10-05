566cbb3ccb0e4bc58b5d90b06085c3f6f3b054cd
palashsarkar07 <psarkar5683@gmail.com>
2025-10-06 00:03:11 +0530

    updated frontend  url

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import aiRoutes from "./routes/ai.route.js"
 const app = express();
 import cors from "cors"
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: "https://codemind-rho.vercel.app", // frontend URL
 }));
 app.use(express.json())
 app.get('/',(req,res)=>{
